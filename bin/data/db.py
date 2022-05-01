510ce00dffc64606f92368e865a11cc3d1b38809
dastkd69 <dastkd27@gmail.com>
2022-05-02 00:25:00 +0530

    Added Database Functionality

--- bin/data/db.py
+++ bin/data/db.py
@@ -3,15 +3,87 @@ import psycopg2
 from typing import Dict
 
 
-def load_connection_info(ini_filename: str) -> Dict[str, str]:
-    parser = ConfigParser()
-    parser.read(ini_filename)
-    # Create a dictionary of the variables stored under the "accounts" section of the .ini
-    conn_info = {param[0]: param[1] for param in parser.items("accounts")}
-    return conn_info
+class bubbledb:
+    def __init__(self):
+        conn_info = self.initialize("db.ini")
+        self.conn = psycopg2.connect(**conn_info)
+        self.cur = self.conn.cursor()
 
 
-def create_db(conn_info: Dict[str, str],) -> None:
+    def initialize(ini_filename: str) -> Dict[str, str]:
+        parser = ConfigParser()
+        parser.read(ini_filename)
+        # Create a dictionary of the variables stored under the "accounts" section of the .ini
+        conn_info = {param[0]: param[1] for param in parser.items("accounts")}
+        return conn_info
+
+
+    def close(self):
+        self.conn.close()
+        self.cur.close()
+    
+
+    def execute_sql(self, query: str) -> None:
+        try:
+            self.cur.execute(query)
+        except Exception as e:
+            print(f"{type(e).__name__}: {e}")
+            print(f"Query: {self.cur.query}")
+            self.conn.rollback()
+            self.cur.close()
+        else:
+            self.conn.commit()
+
+            
+    def create_table(self) -> None:
+        query = """
+                    CREATE TABLE IF NOT EXISTS accounts (
+                    user_id serial PRIMARY KEY,
+                    username VARCHAR(20) UNIQUE NOT NULL,
+                    password VARCHAR(100) NOT NULL,
+                    email VARCHAR(100) UNIQUE NOT NULL,
+                    created_on TIMESTAMP NOT NULL,
+                    last_login TIMESTAMP NOT NULL,
+                    interest1 VARCHAR(50) NOT NULL,
+                    interest2 VARCHAR(50) NOT NULL,
+                    interest3 VARCHAR(50) NOT NULL,
+                    cluster INT UNIQUE NOT NULL);
+                """
+        self.execute_sql(query)
+    
+    
+    def insert(self, data) -> None:
+        query = f"""
+                    INSERT INTO accounts()
+                    VALUES ({data[1]},
+                            {data[2]},
+                            {data[3]},
+                            {data[4]},
+                            {data[5]},
+                            {data[6]},
+                            {data[7]},
+                            {data[8]};
+                """
+        self.execute_sql(query)
+
+    
+    def fetch(self):
+        #logic needs to be implemented depending on how frontend calls for displaying user profiles will be
+        pass
+
+
+    def fetch_interests(self):
+        query = f"""
+                    SELECT interest1, interest2, interest3 FROM accounts
+                """
+        self.execute_sql(query)
+        return self.cur.fetchall()
+
+
+
+
+#This is a special function due to the way psql works
+def create_db(conn_info = bubbledb.initialize()) -> None:
     # Connect just to PostgreSQL with the user loaded from the .ini file
     psql_connection_string = f"user={conn_info['user']} password={conn_info['password']}"
     conn = psycopg2.connect(psql_connection_string)
@@ -31,50 +103,3 @@ def create_db(conn_info: Dict[str, str],) -> None:
         # Revert autocommit settings
         conn.autocommit = False
 
-
-def create_table(sql_query: str, conn: psycopg2.extensions.connection, cur: psycopg2.extensions.cursor) -> None:
-    try:
-        # Execute the table creation query
-        cur.execute(sql_query)
-    except Exception as e:
-        print(f"{type(e).__name__}: {e}")
-        print(f"Query: {cur.query}")
-        conn.rollback()
-        cur.close()
-    else:
-        # To take effect, changes need be committed to the database
-        conn.commit()
-
-
-
-
-
-# host, database, user, password
-conn_info = load_connection_info("db.ini")
-
-# Create the desired database
-create_db(conn_info)
-
-# Connect to the database created
-connection = psycopg2.connect(**conn_info)
-cursor = connection.cursor()
-
-# Create the "house" table
-query = """
-    CREATE TABLE IF NOT EXISTS accounts (
-    user_id serial PRIMARY KEY,
-    username VARCHAR(20) UNIQUE NOT NULL,
-    password VARCHAR(100) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
-    created_on TIMESTAMP NOT NULL,
-    last_login TIMESTAMP NOT NULL,
-    interest1 VARCHAR(50) NOT NULL,
-    interest2 VARCHAR(50) NOT NULL,
-    interest3 VARCHAR(50) NOT NULL,
-    cluster INT UNIQUE NOT NULL);
-"""
-create_table(query, connection, cursor)
-
-# Close all connections to the database
-connection.close()
-cursor.close()
--- bin/models/test.py
+++ bin/models/test.py
@@ -3,10 +3,7 @@ from sklearn.datasets import make_blobs
 from sklearn.preprocessing import StandardScaler
 
 def get():
-    centers = [[1, 1], [-1, -1], [1, -1]]
-    X, labels_true = make_blobs(
-        n_samples=750, centers=centers, cluster_std=0.4, random_state=0
-    )
+    X, labels_true = make_blobs(n_samples=750)
 
     X = StandardScaler().fit_transform(X)
     return X, labels_true
